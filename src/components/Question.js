0bb145918d1bfab0b55a3dc49948697689e2fb45
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-07-25 14:01:18 +0300

    complete assignment

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(()=>{
+   const timer =  setTimeout(()=>{ 
+    setTimeRemaining(timeRemaining -1);}, 1000);
+    onAnswered(false);
+    return ()=> {
+      if(timer){
+        clearTimeout(timer)
+      }
+    }}) 
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
